28230e9a2b7a6206067f955a901177e4b9355282
Aryan Raut <aryan@Aryans-MacBook-Air.local>
2025-07-26 10:09:02 +0530

    created Java intro folder, and added first code

---
+++ L2: Java Intro/codes/Prime_no.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Prime_no {
+    public static void main(String[] args) {
+        System.out.print("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        long num = sc.nextLong();
+        int count = 0;
+
+        for (long i = 1; i<=(num); i++){
+            if(num%i == 0){
+                count++;
+                System.out.print(i + " ");
+            }
+        }
+
+        if(count==2){
+            System.out.println("prime");
+        }
+        else if (count == 1){
+            System.out.println("special");
+        }else {
+            System.out.println("composite");
+        }
+    }
+}
